ff57f3e0c926810ab43c146ea8b500df4c96c824
Andre Rocco <andre.amaral.rocco@gmail.com>
2022-09-02 19:51:22 -0300

    Controlador atualizado

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from curses.ascii import isdigit
 from Game import Game
 from Word import Word
 
@@ -7,3 +8,10 @@ while True:
 
     jogo = Game(palavra_gerada)
     jogo.run()
+
+    continuar = str(input("Deseja jogar mais uma rodada? [S/N] ")).upper()
+    while continuar != 'S' or continuar != 'N':
+        continuar = str(input("Entrada inválida! Deseja jogar mais uma rodada? [S/N] ")).upper()
+    
+    if continuar == 'N':
+        break
