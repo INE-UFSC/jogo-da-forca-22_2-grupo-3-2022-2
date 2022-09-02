a17aedcfe8dcade02dc16f8cd54c7145c550613d
Andre Rocco <andre.amaral.rocco@gmail.com>
2022-09-02 10:49:57 -0300

    Criada classe construtora

--- Game.py
+++ Game.py
@@ -1,2 +1,11 @@
 class Game():
-    def __init__(self): pass
+    def __init__(self, palavra):
+        self.palavra = [*palavra.upper()]
+        
+        self.erros = 0
+        self.tentativas = 0
+        self.vitoria = False
+        self.finalizou = False
+        self.letras_entradas = []
+        self.letras_alfabeto = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    
